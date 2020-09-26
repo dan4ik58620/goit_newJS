50faef693ad7d80ae9778074fbccbdac0631ec66
dan4ik58620 <danyakotkot@gmail.com>
2020-09-26 10:31:15 +0300

    first commit

---
+++ wp_base/src/index.js
@@ -0,0 +1,18 @@
+import css from "./css/style.css";
+// import './module-7/module-7.js';
+import './module-10/module-10.js';
+// import template from './templates/post.hbs';
+// console.log(template);
+// import posts from './data/posts.json';
+// console.log(posts)
+
+// // получаем доступ к кеонтейнеру, куда надо встраивать посты
+// const postsList = document.querySelector(".posts")
+// console.log(postsList)
+
+// // генерируем элемент по шаблону
+// const postItem = template(posts);
+// console.log(postItem)
+
+// // встраиваем сгенерированные посты в контейнер
+// postsList.insertAdjacentHTML('afterbegin', postItem)
